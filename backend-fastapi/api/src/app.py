81ef94032a2e465a225bdbb7dd52c1e957e1d7cd
Кириенко Денис <kirden04@gmail.com>
2023-12-31 04:15:18 +0200

    pre-commit formatting test

--- backend-fastapi/api/src/app.py
+++ backend-fastapi/api/src/app.py
@@ -1,14 +1,13 @@
-import ujson
 from fastapi import FastAPI
-from src.v1.api import router
 from fastapi.middleware.cors import CORSMiddleware
+from src.v1.api import router
 
 app = FastAPI()
 
 origins = [
     "http://localhost:9000",
     "http://localhost:8005",
-    'https://onlinestore.poslam.ru'
+    "https://onlinestore.poslam.ru",
 ]
 
 app.add_middleware(
@@ -18,11 +17,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.on_event("startup")
-async def startup_event():
-    app.json_encoder = ujson
-
 
 app.include_router(router, prefix="/api/v1")
