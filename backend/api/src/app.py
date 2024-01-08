54ec6d8e3201f07473d0c71e3adbaf2565bfa5ae
Кириенко Денис <kirden04@gmail.com>
2024-01-08 15:44:52 +1000

    cors

--- backend/api/src/app.py
+++ backend/api/src/app.py
@@ -4,18 +4,17 @@ from src.v1.api import router
 
 app = FastAPI()
 
-# origins = [
-#     "http://127.0.0.0:9000",
-#     "http://127.0.0.0:8005",
-#     "https://onlinestore.poslam.ru",
-# ]
+origins = [
+    "http://127.0.0.0:9000",
+    "http://127.0.0.0:8005",
+    "https://onlinestore.poslam.ru",
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix="/api/v1")
