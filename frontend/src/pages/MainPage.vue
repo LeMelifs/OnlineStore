95e0c0e80c74e462b04280395e245782df9f5562
LeMelifs <vera_mij_xxx@mail.ru>
2024-01-09 01:52:55 +1000

    Пофиксила баги со страницами авторизации и регистрации, настроила вывод ошибок, убрала лишний функионал, начала верстку страницы "о нас"

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,6 +1,12 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <HeaderComponent/>
+
+        <q-page-container>
+          <HeaderComponent/>
+           <div class="text-weight-bold text-grey-10 text-h5 parent" style="position: fixed; text-align: center; left: 50%; top: 10%; transform: translate(-50%, 0);">
+        Добро пожаловать!
+      </div>
+    </q-page-container>
     <FooterComponent/>
   </q-layout>
 </template>
@@ -11,6 +17,7 @@
 <script setup>
 import HeaderComponent from "components/HeaderComponent.vue";
 import FooterComponent from "components/FooterComponent.vue";
+import BucketOrder from "components/BucketOrder.vue";
 
 
 </script>
