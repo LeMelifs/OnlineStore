7d9010a693ebb60975d232fa12733e22545de6d2
irawrr <ira-koval-2004@mail.ru>
2024-01-11 14:39:24 +1000

    Исправила ошибку

--- frontend/src/components/ProductComponent.vue
+++ frontend/src/components/ProductComponent.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="row parent" >
-    <div style="width: 20%; height: 300px" class="q-mx-sm">
-      <div class="bg-brown-2 q-mx-md q-mr-sm q-pa-xl parent" style="border-radius: 15px">
-        <q-icon size="180px" color="dark" name="mood" />
-      </div>
-      <div class="text-weight-bold text-grey-10 text-h6 q-mt-md" style="margin-left: 21px">
-        Футболка смайл фэйс
-      </div>
-      <div class="text-grey-6  q-mt-xs" style="margin-left: 21px">
-        Удобная футболка вообще темка вещь
-      </div>
-      <div class="text-weight-bold text-grey-10 text-h6 q-mt-xs" style="margin-left: 21px">
-        150 ₽
-      </div>
+  <div style="width: 20%; height: 300px" class="q-mx-sm">
+    <div class="bg-brown-2 q-mx-md q-mr-sm q-pa-xl parent" style="border-radius: 15px">
+      <q-icon size="180px" color="dark" name="mood" />
     </div>
+    <div class="text-weight-bold text-grey-10 text-h6 q-mt-md" style="margin-left: 21px">
+      Футболка смайл фэйс
+    </div>
+    <div class="text-grey-6  q-mt-xs" style="margin-left: 21px">
+      Удобная футболка вообще темка вещь
+    </div>
+    <div class="text-weight-bold text-grey-10 text-h6 q-mt-xs" style="margin-left: 21px">
+      150 ₽
+    </div>
+  </div>
 </template>
 
 <style>
